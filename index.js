b249b395b8d115118a5adf24dd41cef506bdea24
Chase090 <carloyam21@icloud.com>
2021-07-03 02:46:13 -0700

    commit tree 907a9f6b9ef322330052e15428b0b5b7f4f9e053

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
+const greeting = document.getElementById('greeting')
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function displayMessage(message){
+  greeting.innerText = message;
+}
+function greet(string) {
+  let p = string.split(":");
+  let time = parseInt(p);
+
+  if(time < 12) {
+    return `Good Morning`;
+  }
+  if(time > 17) {
+    return `Good Evening`;
+  }
+  else {
+    return `Good Afternoon`;
+  }
+}
